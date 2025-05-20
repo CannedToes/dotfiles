c374ffb0f08b31abb9b178d5457a7df2d4f9b95f
CannedToes <mylesglanville@gmail.com>
2025-05-21 00:48:59 +0200

    install script for linux v2

--- install.sh
+++ install.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
 # Set variables
-REPO="./"
-PACKAGES="neovim starship fish fisher wezterm kitty rustup rust-analyzer netbeans"
-AURPACKAGES="zen-browser-bin"
+REPO="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+PACKAGES="neovim starship fish fisher wezterm kitty zoxide eza keychain ttf-roboto-mono-nerd"
+AURPACKAGES="mkinitcpio-firmware update-grub"
 
+# Confirmation message
+echo -e "Please backup your dotfiles if you want to keep them (obviously)\n"
 echo "This install script is for ARCH LINUX and ARCH LINUX ONLY! If you install it on anything else you are nothing short of shit for brains."
+
+echo "Script Location:" $REPO
 
 read -p "Continue? (Y/N): " confirm && [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]] || exit 1
 
-echo "Please backup your dotfiles if you want to keep them (obviously)"
 git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si
@@ -18,16 +21,29 @@ cd ..
 
 sudo pacman -Syu --needed $PACKAGES
 yay -Syu --needed $AURPACKAGES
-rustup default stable
 
-mkdir $HOME/.config/starship
+clear
 
+# copying across and setting up config files
 cp -r $REPO/universal/nvim $HOME/.config/
 cp -r $REPO/universal/wezterm $HOME/.config/
-cp -r $REPO/universal/starship/linuxStarship.toml $HOME/.config/starship/starship.toml
+cp -r $REPO/universal/starship/linuxStarship.toml $HOME/.config/starship.toml
 
 cp -r $REPO/linux/wezterm.lua $HOME/.config/wezterm/
 cp -r $REPO/linux/fish $HOME/.config/
 
+chsh -s /usr/bin/fish
+nvim $REPO/universal/msgnvim.txt
+
+clear
+
+# cleaning up the directories and packages
+echo "Cleaning up..."
+sudo pacman -Scc
+yay -Scc
+rm -rf $HOME/.cache/*
+rm -rf $REPO/yay
+
+# Installation has completed!
 echo "Installation has finished, enjoy! :)" && sleep 2
 source $HOME/.config/fish/config.fish
