fe9ad39e00a834290b5bf58af323a681eedab4ff
CannedToes <mylesglanville@gmail.com>
2025-05-24 23:16:33 +0200

    fully implemented first hyprland config

--- install.sh
+++ install.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 # Set variables
 REPO="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-PACKAGES="git neovim fzf starship fish fisher kitty zoxide eza keychain nerd-fonts"
-AURPACKAGES="mkinitcpio-firmware wezterm-git"
-FISHERPACKAGES="Jomik/fish-gruvbox jethrokuan/fzf jorgebucaran/bax.fish jorgebucaran/nvm.fish franciscolourenco/done jorgebucaran/spark.fish laughdelic/pisces joseluisq/gitnow adambrenecki/virtualfish Gazorby/fish-abbreviation-tips"
-HYPRPACKAGES="hyprland nwg-look qt5ct qt6ct pipewire wireplumber xdg-desktop-portal-hyprland hyprpolkitagent qt5-wayland qt6-wayland python-pywal16 waybar"
+PACKAGES="git neovim fzf starship fish fisher kitty zoxide eza nerd-fonts hyprland sddm swaync nwg-look qt5ct qt6ct pipewire wireplumber xdg-desktop-portal-hyprland hyprpolkitagent qt5-wayland qt6-wayland python-pywal16 waybar wofi"
+AURPACKAGES="mkinitcpio-firmware wezterm-git wlogout swaylock-effects waybar gtk-engine-murrine gtk-murrine-themes-collection graphite-gtk-theme"
+FISHERPACKAGES="Jomik/fish-gruvbox jethrokuan/fzf jorgebucaran/bax.fish jorgebucaran/nvm.fish franciscolourenco/done jorgebucaran/spark.fish laughdelic/pisces adambrenecki/virtualfish Gazorby/fish-abbreviation-tips"
+HYPRREPOS="https://github.com/VirtCode/hypr-dynamic-cursors https://github.com/hyprwm/hyprland-plugins"
+HYPRPACKAGES="dynamic-cursors borders-plus-plus csgo-vulkan-fix hyprtrails"
 
 # Confirmation message
 echo -e "Please backup your dotfiles if you want to keep them (obviously)\n"
 echo "This install script is for ARCH LINUX and ARCH LINUX ONLY! If you install it on anything else you are nothing short of shit for brains."
+echo "Also you must have sudo privileges to be able to run this script, as it will ask you for such"
 echo "Script Location:" "$REPO"
 
 read -p "Continue? (Y/N): " confirm && [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]] || exit 1
@@ -18,7 +20,6 @@ git pull --all
 # check if yay is installed and install it if not
 YAYINSTALLED="$(pacman -Q yay)"
 if ["$YAYINSTALLED" = "error: package 'yay' was not found"]; then
-  echo "$YAYINSTALLED"
   git clone https://aur.archlinux.org/yay.git
   cd yay
   makepkg -si
@@ -32,15 +33,18 @@ fisher install "$FISHERPACKAGES"
 clear
 
 # copying across and setting up config files
-cp -r "$REPO"/universal/nvim "$HOME"/.config/
+mkdir -p "$HOME"/.config
+cp -rf "$REPO"/universal/nvim "$HOME"/.config/
+mkdir -p "$HOME"/Pictures
+cp -rf "$REPO"/universal/Wallpapers "$HOME"/Pictures/
 
-cp -r "$REPO"/linux/starship.toml "$HOME"/.config/
-cp -r "$REPO"/linux/wezterm "$HOME"/.config/
-cp -r "$REPO"/linux/fish "$HOME"/.config/
+cp -rf "$REPO"/linux/* "$HOME"/.config/
 
 # update plugins
 chsh -s /usr/bin/fish
 fisher update
+hyprpm add "$HYPRREPOS"
+hyprpm enable "$HYPRPACKAGES"
 nvim "$REPO"/universal/msgnvim.txt
 
 clear
@@ -49,10 +53,11 @@ clear
 echo "Cleaning up..."
 sudo pacman -Scc
 rm -rf "$HOME"/.cache/*
-sudo rm -rf "$REPO"/yay
+rm -rf "$REPO"/yay
 
 # Installation has completed!
 echo "hint - run :Mason in nvim to configure coding languages"
 echo -e "hint - run :Lazy in nvim to configure plugins\n"
-echo "Installation has finished enjoy! :)" && sleep 2
+echo "Restart for full changes to take effect"
+echo "Installation has completed enjoy! :)" && sleep 2
 fish
