cd1695ea0a2c7fe51c8553a4c2d985c81db9dfa3
CannedToes <mylesglanville@gmail.com>
2025-05-27 08:42:06 +0200

    fixed

---
+++ universal/nvim/lua/options.lua
@@ -0,0 +1,9 @@
+require "nvchad.options"
+
+-- add yours here!
+
+vim.o.cursorlineopt ='both' -- to enable cursorline!
+vim.o.tabstop = 4
+vim.o.expandtab = true
+vim.o.softtabstop = 4
+vim.o.shiftwidth = 4
