85edd189271b15a9aea216426d62880833b7f2e4
Canto <mylesglanville@gmail.com>
2025-05-30 21:47:16 +0200

    windows and switched nvim base AGAIN

--- windows/wezterm/lua/appearance.lua
+++ windows/wezterm/lua/appearance.lua
@@ -2,17 +2,18 @@
 local M = {}
 
 M.apply_to_config = function(config)
-  config.color_scheme = "GruvboxDark"
-  config.font = Wezterm.font("RobotoMono Nerd Font")
-  config.font_size = 14
-  config.window_background_opacity = 0.80
-  config.enable_tab_bar = true
-  config.use_fancy_tab_bar = true
-  config.max_fps = 120
-  -- config.kde_window_background_blur = true
-  config.window_decorations = "NONE"
-  config.window_close_confirmation = "NeverPrompt"
-  config.default_cursor_style = "BlinkingBar"
+	config.color_scheme = "Gruvbox dark, hard (base16)"
+	config.font = Wezterm.font("RobotoMono Nerd Font", { weight = "Medium" })
+	config.font_size = 12
+	config.window_background_opacity = 1.00
+	config.enable_tab_bar = true
+	config.use_fancy_tab_bar = true
+	config.hide_tab_bar_if_only_one_tab = true
+	config.max_fps = 120
+	-- config.kde_window_background_blur = true
+	config.window_decorations = "RESIZE"
+	config.window_close_confirmation = "NeverPrompt"
+	config.default_cursor_style = "BlinkingBar"
 end
 
 return M
--- windows/wezterm/wezterm.lua
+++ windows/wezterm/wezterm.lua
@@ -4,6 +4,22 @@ Wezterm = require("wezterm")
 -- This will hold the configuration.
 local config = Wezterm.config_builder()
 
+-- Apply on GUI start
+Wezterm.on("gui-startup", function(cmd)
+	local screen = Wezterm.gui.screens().active
+	local ratio = 0.75
+	local width, height = screen.width * ratio, screen.height * ratio
+	local tab, pane, window = Wezterm.mux.spawn_window({
+		position = {
+			x = (screen.width - width) / 2,
+			y = (screen.height - height) / 2,
+			origin = "ActiveScreen",
+		},
+	})
+	-- window:gui_window():maximize()
+	window:gui_window():set_inner_size(width, height)
+end)
+
 -- This is where you actually apply your config
 local appearance = require("lua.appearance")
 appearance.apply_to_config(config)
