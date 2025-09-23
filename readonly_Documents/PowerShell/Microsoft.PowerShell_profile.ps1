79bc2f620da489f1cb990408c94bbd69ad47835e
Myles Glanville <mylesglanville@gmail.com>
2025-09-23 12:49:56 +0200

    updated

--- readonly_Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++
@@ -1,7 +0,0 @@
-Invoke-Expression (&starship init powershell)
-Invoke-Expression (& { (zoxide init powershell | Out-String) })
-
-Set-Alias cd z -Option AllScope
-
-Set YOLK_DIR C:\Users\Myles\Dotfiles
-Set EDITOR nvim
