5fdc8e51705cc09e038bb902a03b50a6bbb58244
Myles Glanville <mylesglanville@gmail.com>
2025-09-30 09:22:51 +0200

    Update Documents/PowerShell/Microsoft.PowerShell_profile.ps1

--- Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,7 +1,29 @@
-Invoke-Expression (&starship init powershell)
-Invoke-Expression (& { (zoxide init powershell | Out-String) })
-
-Set-Alias cd z -Option AllScope
-
-Set YOLK_DIR C:\Users\Myles\Dotfiles
+# environments variables
 Set EDITOR nvim
+Set NVIM_CONF "C:/Users/myles/AppData/Local/nvim/"
+
+# inits & plugins
+Invoke-Expression (& { (starship init powershell | Out-String) })
+Invoke-Expression (& { (zoxide init powershell | Out-String) })
+Invoke-Expression (& { (chezmoi completion powershell | Out-String) })
+
+Import-Module PSReadLine -ErrorAction SilentlyContinue
+
+# specific config
+  # PSReadLine
+  Set-PSReadlineKeyHandler -Chord Ctrl+F -Function MenuComplete
+  Set-PSReadLineOption -HistorySearchCursorMovesToEnd
+  Set-PSReadLineKeyHandler -Key UpArrow -Function HistorySearchBackward
+  Set-PSReadLineKeyHandler -Key DownArrow -Function HistorySearchForward
+  Set-PSReadLineOption -PredictionSource History
+  Set-PSReadLineOption -ShowToolTips
+
+# functions
+function lseza {
+  eza -l --icons --no-permissions
+}
+
+# aliases
+Set-Alias cd z -Option AllScope
+Set-Alias ls lseza
+Set-Alias which Get-Command
