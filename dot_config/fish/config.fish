128fab180ea15454eadaa860713c1aba724d7dd7
CannedToes <mylesglanville@gmail.com>
2026-02-02 09:18:08 +0200

    zsh

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -1,3 +1,6 @@
-if status is-interactive
-  starship init fish | source
-end
+set fish_greeting "Yabagoobie!"
+
+starship init fish | source
+zoxide init fish | source
+
+fish_add_path ~/.config/emacs/bin
