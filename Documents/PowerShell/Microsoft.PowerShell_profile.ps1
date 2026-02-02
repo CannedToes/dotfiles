8c42a4103fbfd240865a821ebb8abf7782838120
CannedToes <mylesglanville@gmail.com>
2026-02-02 13:11:32 +0200

    powershell removed eza

--- Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -4,9 +4,8 @@ Set NVIM_CONF "C:/Users/myles/AppData/Local/nvim/"
 
 # inits & plugins
 Invoke-Expression (& { (starship init powershell | Out-String) })
+Invoke-Expression (& { (zoxide init powershell | Out-String) })
 Invoke-Expression (& { (chezmoi completion powershell | Out-String) })
-
-Import-Module PSReadLine -ErrorAction SilentlyContinue
 
 # specific config
   # PSReadLine
@@ -15,14 +14,7 @@ Import-Module PSReadLine -ErrorAction SilentlyContinue
   Set-PSReadLineKeyHandler -Key UpArrow -Function HistorySearchBackward
   Set-PSReadLineKeyHandler -Key DownArrow -Function HistorySearchForward
   Set-PSReadLineOption -PredictionSource History
-  Set-PSReadLineOption -ShowToolTips
-
-# functions
-function lseza {
-  eza -l --icons --no-permissions $args
-}
 
 # aliases
 Set-Alias neofetch fastfetch
-Set-Alias ls lseza
 Set-Alias which Get-Command
